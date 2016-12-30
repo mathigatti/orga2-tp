88be236da54ea24109fc1badeb99a55a1038d4e4
manucosta <manucos94@gmail.com>
2016-12-30 12:47:32 -0300

    Agregué random shuffle

--- src/c/main.c
+++ src/c/main.c
@@ -120,6 +120,18 @@ double* matrix_vec_prod(double* W, double* x, uint rows, uint cols){
   return y;
 }
 
+void shuffle(int *array, size_t n) {
+  if (n > 1) {
+    size_t i;
+    for (i = 0; i < n - 1; i++) {
+      size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+      int t = array[j];
+      array[j] = array[i];
+      array[i] = t;
+    }
+  }
+}
+
 int main(){
   Network* net = (Network*) malloc(sizeof(Network));
   initialize_net(net, 1);
